f8c03431cb039419711c232296e71b0f8a4e1cba
Julian Zucker <julian.zucker@gmail.com>
2017-06-25 16:07:52 -0400

    Parsed html from evals

--- scraper.R
+++ scraper.R
@@ -70,7 +70,7 @@ getReportURLs <- function() {
         document.getElementById("contentFrame").contentWindow.document, null, XPathResult.ANY_TYPE, null);
     var n = totals.iterateNext();
     return n.innerHTML;') %>%
-    `[[`(1) %>%
+    extract2(1) %>%
     str_extract("[0-9]+(?= total)") %>%
     as.numeric()
   
@@ -120,7 +120,8 @@ getReportURLs <- function() {
     unname() %>%
     matrix(ncol=4, byrow=TRUE)
   linkDF <- cbind(info, links)
-  linkDF <- as.data.frame(linkDF)
+  linkDF <- as.data.frame(linkDF) %>%
+    lapply(as.character)
   colnames(linkDF) <- c("Term", "CourseNumber", "CourseName", "Instructor", "Link")
   return(linkDF)
 }
@@ -130,7 +131,7 @@ getReportURLs <- function() {
 # just too damn big to anything reasonable with.
 # Args: character vector representing local links on applyweb.com to be scraped
 getTraceEvals <- function(links) {
-  curLink <- 0 # Zero indexed because modulo starts at 0, so add one to work with R
+  curLink <- 0 # Zero indexed because %% starts at 0, so add one to work with R
   chunkSize <- 100
   baseURL <- "https://www.applyweb.com"
   evals <- list(mode="character", length=chunkSize)
@@ -145,7 +146,7 @@ getTraceEvals <- function(links) {
     dr$navigate(str_c(baseURL, link))
     
     # I'm not doing any parsing here because evals for different programs
-    # have different formats
+    # have different formats, and I want the scraper to be general
     eval <- getIFrameSource()
     evals[[(curLink %% chunkSize) + 1]] <- eval
     
@@ -180,7 +181,148 @@ csClassLinks <- linkDF %>%
   filter(str_detect(CourseNumber, '^(CS|IS|DS|DSCS|IA)[0-9]{4,}')) %>%
   .$Link
 
-
 getTraceEvals(csClassLinks)
+
+
+load("evals1")
+eval <- evals[[1]]
+
+
+
+parseTraceEval <- function(eval) {
+  out <- vector(mode="character")
+  names(out) <- c("Course Name",
+                  "Instructor",
+                  "Section",
+                  "Course Title",
+                  "CRN",
+                  "Subject",
+                  "Enrollment",
+                  "Responses",
+                  "Declines",
+                  "Department",
+                  "Number",
+                  "Course Related Questions Course",
+                  "Course Related Questions Department",
+                  "Course Related Questions University",
+                  "Learning Related Questions Course",
+                  "Learning Related Questions Department",
+                  "Learning Related Questions University",
+                  "Instructor Related Questions Course",
+                  "Instructor Related Questions Department",
+                  "Instructor Related Questions University",
+                  "Instructor Effectiveness Course",
+                  "Instructor Effectiveness Department",
+                  "Instructor Effectiveness University",
+                  "Syllabus Helped Course", 
+                  "Syllabus Helped Department", 
+                  "Syllabus Helped University", 
+                  "Textbook Helped Course", 
+                  "Textbook Helped Department", 
+                  "Textbook Helped University",
+                  "Online Helped Course", 
+                  "Online Helped Department", 
+                  "Online Helped University", 
+                  "Assignments Helped Course", 
+                  "Assignments Helped Department", 
+                  "Assignments Helped University",
+                  "Lectures Helped Course", 
+                  "Lectures Helped Department", 
+                  "Lectures Helped University", 
+                  "Inclass Helped Course", 
+                  "Inclass Helped Department", 
+                  "Inclass Helped University", 
+                  "Classroom Technology Helped Course", 
+                  "Classroom Technology Helped Department", 
+                  "Classroom Technology Helped University", 
+                  "Course Was Intellectual Course", 
+                  "Course Was Intellectual Department", 
+                  "Course Was Intellectual University",
+                  "Learned a Lot Course", 
+                  "Learned a Lot Department",
+                  "Learned a Lot University", 
+                  "Learned Concepts And Principles Course", 
+                  "Learned Concepts And Principles Department", 
+                  "Learned Concepts And Principles University", 
+                  "Developed Skills Expressing Course", 
+                  "Developed Skills Expressing Department",
+                  "Developed Skills Expressing University", 
+                  "Learned Analyze and Evaluate Course", 
+                  "Learned Analyze and Evaluate Department", 
+                  "Learned Analyze and Evaluate University",
+                  "Instructor Communication Skills Course", 
+                  "Instructor Communication Skills Department", 
+                  "Instructor Communication Skills University",
+                  "Instructor Communicated Course", 
+                  "Instructor Communicated Department", 
+                  "Instructor Communicated University", 
+                  "Instructor Stated Objectives Course",
+                  "Instructor Stated Objectives Department", 
+                  "Instructor Stated Objectives University",
+                  "Instructor Covered Stated Course", 
+                  "Instructor Covered Stated Department", 
+                  "Instructor Covered Stated University", 
+                  "Instructor Prepared Course", 
+                  "Instructor Prepared Department", 
+                  "Instructor Prepared University",
+                  "Instructor Used Class Time Well Course", 
+                  "Instructor Used Class Time Well Department", 
+                  "Instructor Used Class Time Well University", 
+                  "Instructor Provided Feedback Course", 
+                  "Instructor Provided Feedback Department", 
+                  "Instructor Provided Feedback University",
+                  "Instructor Fairly Evaluated Performance Course", 
+                  "Instructor Fairly Evaluated Performance Department",
+                  "Instructor Fairly Evaluated Performance University", 
+                  "Would Recommend Course", 
+                  "Would Recommend Department",
+                  "Would Recommend University", 
+                  "Respected Students Course", 
+                  "Respected Students Department",
+                  "Respected Students University",
+                  "Instructor Took Effective Action Course",
+                  "Instructor Took Effective Action Department", 
+                  "Instructor Took Effective Action University",
+                  "Instructor Available Outside of Class Course", 
+                  "Instructor Available Outside of Class Department",
+                  "Instructor Available Outside of Class University", 
+                  "Instructor Displayed Enthusiasm Course", 
+                  "Instructor Displayed Enthusiasm Department", 
+                  "Instructor Displayed Enthusiasm University",
+                  "Instructor Overall Course",
+                  "Instructor Overall Department",
+                  "Instructor Overall University")
+  
+  tree <- htmlParse(eval, useInternalNodes = TRUE)
+  
+  name <- xmlValue(getNodeSet(tree, '/html/body/div[1]/div[2]/div/h3')[[1]])
+  course_data <- getNodeSet(tree, '/html/body/div[1]/div[2]/div/div/div[1]/ul/li/strong') %>%
+    xmlSApply(xmlValue) 
+  survey_response <- getNodeSet(tree, '/html/body/div[1]/div[2]/div/div/div[2]/ul/li/strong') %>%
+    xmlSApply(xmlValue) 
+  
+  processBarChart <- function(xpath) {
+    getNodeSet(tree, xpath) %>%
+      xmlSApply(function(x) xmlSApply(x, xmlValue) %>% str_extract("[0-9]\\.[0-9]")) %>%
+      unlist() %>% t()
+  }
+  
+  category_summary <- processBarChart('//*[@id="bar_mean_cat_1"]/div/div[1]/div/svg/g[2]/g[4]')
+  course_related_questions <- processBarChart('//*[@id="bar_mean_2_1"]/div/div[1]/div/svg/g[2]/g[4]')
+  learning_related_questions <- processBarChart('//*[@id="bar_mean_2_2"]/div/div[1]/div/svg/g[2]/g[4]')
+  instructor_related_questions <- processBarChart('//*[@id="bar_mean_2_3"]/div/div[1]/div/svg/g[2]/g[4]')
+  instructor_overall <- processBarChart('//*[@id="bar_mean_2_22"]/div/div[1]/div/svg/g[2]/g[4]')
+  
+  times <- c("1-4", "5-8", "9-12", "13-16", "17-20")
+  
+  percents <- xmlSApply(getNodeSet(tree, '//*[@id="pie_3_9"]/svg/g[3]'), xmlValue)
+  labels <- xmlSApply(getNodeSet(tree, '//*[@id="pie_3_9"]/svg/g[4]'), xmlValue)
+  
+  out <- c(name, course_data, survey_response, category_summary,
+    course_related_questions, learning_related_questions, instructor_related_questions,
+    instructor_overall)
+}
+
+
 
 mybrowser$server$stop()
